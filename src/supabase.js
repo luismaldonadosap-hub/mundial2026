8ce26f509af1c6a5e903920506be74e55118f2b2
Luis E Maldonado Pinto <Luisernestomaldonadopinto@MacBook-Pro-7.local>
2026-06-09 13:59:00 +0200

    Fix Supabase keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://tjuludnubnckcvskaysa.supabase.co',
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqdWx1ZG51Ym5ja2N2c2theXNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3Njc5MDUsImV4cCI6MjA5NTM0MzkwNX0.-nBPktsytfK7Ums-FHoVn-FOfmFaDz-tAC8ssoH-S7Y'
 )
